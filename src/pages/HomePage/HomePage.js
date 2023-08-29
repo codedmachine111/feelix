0b7bf174015180640558a54f96a7bca0d7d14780
codedmachine111 <charlesbabbage1709@gmail.com>
2023-08-29 22:04:10 +0530

    feat: initial upload of project files

---
+++ src/pages/HomePage/HomePage.js
@@ -0,0 +1,10 @@
+import './HomePage.scss'
+
+export const HomePage = ()=>{
+
+    return(<>
+        <div className='homepage-container'>
+            <h2>Welcome!</h2>
+        </div>
+    </>)
+}
---
+++ src/pages/LandingPage/LandingPage.js
@@ -0,0 +1,20 @@
+import "./LandingPage.scss";
+
+export const LandingPage = () => {
+  return (
+    <>
+      <div className="landingpage-container">
+        <div className="landingpage-content">
+          <h2>Login to Your Account</h2>
+          <p>
+            Experience engaging video chats enhanced with real-time emotion
+            prediction, bringing a new level of connection to your conversations.
+          </p>
+        </div>
+        <div className="landingpage-form-container">
+            
+        </div>
+      </div>
+    </>
+  );
+};

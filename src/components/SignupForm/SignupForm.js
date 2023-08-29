ae905b0a2cf5f5f34c443674e2e3033fa7e242bc
codedmachine111 <charlesbabbage1709@gmail.com>
2023-08-30 01:12:41 +0530

    feat: user signup

---
+++ src/components/SignupForm/SignupForm.js
@@ -0,0 +1,75 @@
+import "./SignupForm.scss";
+import { useState, useCallback } from "react";
+import axios from "axios";
+
+export const SignupForm = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [username, setUsername] = useState("");
+
+  const resetForm = () => {
+    setEmail("");
+    setPassword("");
+    setUsername("");
+  };
+
+  const handleSignupForm = useCallback(
+    (e) => {
+      e.preventDefault();
+
+      axios.post('http://localhost:3001/auth/signup', {email: email, username: username, password: password}).then((res)=>{
+        if(res.data.message == "User Created Successfully!"){
+            resetForm();
+            alert("User Created Successfully!")
+        }
+      })
+    },
+    [email, password, username]
+  );
+
+  return (
+    <>
+      <form id="signup-form" onSubmit={handleSignupForm}>
+        <label htmlFor="username">Name:</label>
+        <input
+          type="text"
+          name="username"
+          id="username"
+          value={username}
+          onChange={(e) => {
+            setUsername(e.target.value);
+          }}
+          autoComplete="off"
+        />
+
+        <label htmlFor="email">Email:</label>
+        <input
+          type="text"
+          name="email"
+          id="email"
+          value={email}
+          onChange={(e) => {
+            setEmail(e.target.value);
+          }}
+          autoComplete="off"
+        />
+
+        <label htmlFor="password">Password:</label>
+        <input
+          type="password"
+          name="password"
+          id="password"
+          value={password}
+          onChange={(e) => {
+            setPassword(e.target.value);
+          }}
+          autoComplete="off"
+        />
+
+        <button type="submit" onSubmit={handleSignupForm}>
+          Signup
+        </button>
+      </form>
+    </>
+  );
+};
--- src/pages/LandingPage/LandingPage.js
+++ src/pages/LandingPage/LandingPage.js
@@ -1,3 +1,4 @@
+import { SignupForm } from "../../components/SignupForm/SignupForm";
 import "./LandingPage.scss";
 
 export const LandingPage = () => {
@@ -8,11 +9,15 @@ export const LandingPage = () => {
           <h2>Login to Your Account</h2>
           <p>
             Experience engaging video chats enhanced with real-time emotion
-            prediction, bringing a new level of connection to your conversations.
+            prediction, bringing a new level of connection to your
+            conversations.
           </p>
         </div>
         <div className="landingpage-form-container">
-            
+          <div className="landingpage-form-holder">
+            <SignupForm />
+          </div>
+          <div className="landingpage-options"></div>
         </div>
       </div>
     </>
